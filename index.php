b4e2af7c43e800c53b888121db175f7da317329d
Jesús Rodríguez <info@tecnibyte.com>
2015-10-13 18:35:11 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+<!DOCTYPE html>
+
+    <head>
+    <script type="text/javascript" src="<?php echo $this->baseurl; ?>/templates/mi_template/assets/js/jquery.sticky.js"></script>
+  <script>
+    jQuery.noConflict();	
+    jQuery(window).load(function($){
+      jQuery("#MiIdCss").sticky({ topSpacing: 0});
+    });
+  </script>
+</head>
+<body>
+
+</body>
+</html>

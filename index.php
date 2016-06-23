5f64466c7187d86f1271f68157be20d74e68b535
Jesús Rodríguez <info@tecnibyte.com>
2016-06-23 19:28:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,20 @@
 
     <head>
     <script type="text/javascript" src="<?php echo $this->baseurl; ?>/templates/mi_template/assets/js/jquery.sticky.js"></script>
-  <script>
-    jQuery.noConflict();	
-    jQuery(window).load(function($){
-      jQuery("#MiIdCss").sticky({ topSpacing: 0});
-    });
-  </script>
+<script type="text/javascript">
+jQuery.noConflict();
+jQuery(document).ready(function() {
+if (jQuery( window ).width()>=780) {
+				jQuery("#miclasecss").sticky({ topSpacing: 0});
+			}
+jQuery(window).resize(function() {
+		if (jQuery( window ).width()>=780) {
+				jQuery("#miclasecss").sticky({ topSpacing: 0});
+			}
+});
+});
+
+</script>  
 </head>
 <body>
 
